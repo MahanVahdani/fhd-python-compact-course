f101a984cbcdf1f0c23eb5b66197446efdaa49b1
MahanVahdani <75772432+MahanVahdani@users.noreply.github.com>
2026-09-20 16:42:44 +0330

    Complete Task 3

---
+++ week1/Task3/main.py
@@ -0,0 +1,26 @@
+# 1. Integer to float
+num_int = 10
+converted_float = float(num_int)
+print("Integer to float:", converted_float, type(converted_float))
+
+
+# 2. Float to integer
+num_float = 10.75
+converted_int = int(num_float)
+print("Float to integer:", converted_int, type(converted_int))
+
+
+# 3. Integer to string
+num_to_str = str(42)
+print("Integer to string:", num_to_str, type(num_to_str))
+
+
+# 4. String to integer
+str_num = "123"
+str_converted_int = int(str_num)
+print("String to integer:", str_converted_int, type(str_converted_int))
+
+
+# 5. Integer to boolean
+bool_val = bool(1)
+print("Integer to boolean:", bool_val, type(bool_val))
