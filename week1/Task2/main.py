2ca170227e12f37e95b1855f72b1fc066245af6c
MahanVahdani <75772432+MahanVahdani@users.noreply.github.com>
2026-09-20 16:27:32 +0330

    Complete Task 2

---
+++ week1/Task2/main.py
@@ -0,0 +1,9 @@
+zahl = 10
+kommazahl = 10.5
+text = "Hello, World!"
+wahrheitswert = True
+
+print("Integer variable (zahl):", zahl, type(zahl))
+print("Float variable (kommazahl):", kommazahl, type(kommazahl))
+print("String variable (text):", text, type(text))
+print("Boolean variable (wahrheitswert):", wahrheitswert, type(wahrheitswert))
